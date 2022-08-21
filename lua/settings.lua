491f26056b078abbe1f9f491f28c76a28159aee5
rainboy <rainboylvx@qq.com>
2022-08-21 08:37:17 +0800

    add baisc setting

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,11 +1,74 @@
+local HOME = os.getenv("HOME")
 local settings = {
-	number = true,
-	relativenumber = true,
-	tabstop = 4,
-	shiftwidth = 4,
-	expandtab = true
-}
+    -- basic settings
+    encoding = "utf-8",
+    backspace = "indent,eol,start", -- backspace works on every char in insert mode
+    completeopt = "menu,noinsert",
+    history = 1000,
+    startofline = true,
 
+    -- Mapping wating time
+    timeout = false,
+    ttimeout = true,
+    ttimeoutlen = 100,
+
+    -- display
+    -- background = "dark",
+    termguicolors = true,
+    showmatch = false , -- briefly jump to the matching one bracket
+    laststatus = 2,     -- always show status line
+    scrolloff = 3, -- always show 3 rows from edge of the screen
+    synmaxcol = 300, -- stop syntax highlight after x lines for performance
+
+    list = true, -- display white characters
+    listchars = 'trail:·,nbsp:◇,tab:→ ,extends:▸,precedes:◂,multispace:→   ',
+--listchars='eol:¬,space:·,lead: ,trail:·,nbsp:◇,tab:→-,extends:▸,precedes:◂,multispace:···⬝,leadmultispace:│   ',
+--listchars='eol:¬,space:·,lead: ,trail:·,nbsp:◇,tab:→-,extends:▸,precedes:◂,multispace:···⬝,leadmultispace:│   ,',
+-- o.formatoptions = 'qrn1' -- help fo-table
+    foldenable = true,
+    foldlevel = 4, -- limit folding to 4 levels
+    foldmethod = 'syntax', -- use language syntax to generate folds
+    wrap = true,
+    showbreak= '↪', -- character to show when line is broken
+
+    -- Sidebar
+	number = true,-- line number on the left
+    numberwidth = 3, -- always reserve 3 spaces for line number
+	relativenumber = true,
+    signcolumn = 'yes', -- keep 1 column for coc.nvim check
+    showcmd = true, -- display command in bottom bar
+    -- Search
+    incsearch = true, -- starts searching as soon as typing, without enter needed
+    ignorecase = true, -- ignore letter case when searching
+    smartcase = true, -- case insentive unless capitals used in search
+
+    matchtime = 2, -- delay before showing matching paren
+    mps = vim.o.mps .. ",<:>",
+
+    -- White characters
+    autoindent = true,
+    smartindent = true,
+    tabstop = 4, -- 1 tab = 2 spaces
+    shiftwidth = 4, -- indentation rule
+    formatoptions = 'qnj1', -- q  - comment formatting; n - numbered lists; j - remove comment when joining lines; 1 - don't break after one-letter word
+    expandtab = true, -- expand tab to spaces
+
+-- Backup files
+    backup = true, -- use backup files
+    writebackup = false,
+    swapfile = false, -- do not use swap file
+    undodir = HOME .. '/.vim/tmp/undo//',    -- undo files
+    backupdir = HOME .. '/.vim/tmp/backup//', -- backups
+    directory = '/.vim/tmp/swap//',   -- swap files
+
+    -- Commands mode
+    wildmenu = true, -- on TAB, complete options for system command
+    wildignore = 'deps,.svn,CVS,.git,.hg,*.o,*.a,*.class,*.mo,*.la,*.so,*.obj,*.swp,*.jpg,*.png,*.xpm,*.gif,.DS_Store,*.aux,*.out,*.toc',
+
+}
 for k,v in pairs(settings) do
-	vim.opt[k] = v
+	vim.o[k] = v
 end
+
+    -- theme
+vim.cmd("colorscheme murphy")
--- readme.md
+++ readme.md
@@ -2,6 +2,15 @@
 
 在vim9 与 neovim 中,我最后还是选择了neovim
 
+## 安装
+
+确保`~/.config/`路径下没有`nvim`文件夹
+
+```bash
+mv ~/.config/nvim ~/.config/nvim_bak
+git clone -b lua-neovim https://github.com/Rainboylvx/RainboyVim ~/.config/nvim
+```
+
 ## 配置过程
 
 - [ ] [学习lua配置][1]
@@ -11,5 +20,10 @@
 ## 资源
 
 - [glepnir/nvim-lua-guide-zh: https://github.com/nanotee/nvim-lua-guide chinese version][1]
+
+参考配置
+
+- [ravenxrz/dotfiles](https://github.com/ravenxrz/dotfiles/blob/master/nvim/)
+- [arnvald/viml-to-lua](https://github.com/arnvald/viml-to-lua)
 
 [1]: <https://github.com/glepnir/nvim-lua-guide-zh>
